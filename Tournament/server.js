26ac6f9214c672ee51f5f17e983343218cb48571
Fitzchak Yitzchaki <fitzchak@ayende.com>
2015-12-06 17:03:36 +0200

    Create matches for the next round

--- Tournament/server.js
+++ Tournament/server.js
@@ -10,10 +10,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-	var players = [
-		{ id: 1, name: "fitzchak" }, 
-		{ id: 2, name: "Adi" }, { id: 3, name: "hila" }, { id: 4, name: "Maxim" }
-	];
+
 
     /*for (var i = 0; i < players.length; i++) {
 		var player = players[i];
@@ -26,17 +23,44 @@ app.get('/', function (req, res) {
 app.get('/new', function (req, res) {
 
 	var tournament = {
-		id="tournament/"
 		name: "",
 		createdAt: "",
-		fetchPlayersFrom: ""
+        fetchPlayersFrom: "",
+        matches:[]
     };
-	
-	var ravenReq = http.request({ host: "localhost", port: 8080, path: "/databases/tournament/docs", method: "PUT" }, function (ack, resp) {
+    
+    var players = [
+        { id: 1, name: "fitzchak" }, 
+        { id: 2, name: "Adi" }, 
+        { id: 3, name: "hila" }, 
+        { id: 4, name: "Maxim" },
+        { id: 5, name: "A" },
+        { id: 6, name: "B" },
+        { id: 7, name: "C" },
+        { id: 8, name: "F" },
+        { id: 9, name: "D2" },
+        { id: 10, name: "Fsd" },
+        { id: 11, name: "Gd" }
+    ];
+
+    players = shuffle(players);
+
+    var matches = [];
+    var closetPowerOf2 = Math.pow(2, Math.ceil(Math.log(players.length) / Math.log(2)));
+    for (var i = 0; i < players.length; i++) {
+        if (i < closetPowerOf2 / 2) {
+            matches.push([players[i], players[i + 1]]);
+            i++;
+        } else {
+            matches.push([players[i], null]);
+        }
+    }
+    tournament.matches = matches;
+
+	var ravenReq = http.request({ host: "localhost", port: 8080, path: "/databases/tournament/docs/tournament/", method: "PUT" }, function (ack, resp) {
 	    var data = [];
 		ack.on('data', function (chunk) {
 		    data.push(chunk);
-	        
 		});
 		ack.on('end', function () {
 		    var result = JSON.parse(data.join(''));
@@ -44,20 +68,47 @@ app.get('/new', function (req, res) {
 		});
 	}).on('error',function(nack) {
 	    res.render("new", [{ id: 1, name: "error: "+ nack.message }]);
-	});
+    });
+
 	ravenReq.write(JSON.stringify(tournament));
     ravenReq.end();
 });
 
-app.get('/tournament/:id', function (req, res) {
-    var id = req.params.id;
-	
-	/*var players = [
-		{ id: 1, name: "fitzchak" }, 
-		{ id: 2, name: "Adi" }, { id: 3, name: "hila" }, { id: 4, name: "Maxim" }
-		];*/
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    
+    return array;
+}
 
-	res.render("tournament", {id: id});
+app.get('/tournament/*', function (req, res) {
+    var id = req.params[0];
+    var result = null;
+    var ravenReq = http.request({ host: "localhost", port: 8080, path: "/databases/tournament/docs/"+ id, method: "GET" }, function (ack, resp) {
+        var data = [];
+        ack.on('data', function (chunk) {
+            data.push(chunk);
+        });
+        ack.on('end', function () {
+            result = JSON.parse(data.join(''));
+            res.render("tournament", { id: id });
+        });
+    }).on('error', function (nack) {
+        res.redirect("new" + result.Key);
+    });
+    ravenReq.end();
 });
 
 var server = app.listen(port, function () {
@@ -66,57 +117,6 @@ var server = app.listen(port, function () {
 	
 	console.log('Example app listening at http://%s:%s', host, port);
 });
-
-/*http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Worfld\n');
-}).listen(port);*/
-
-
-console.log('Hello world');
-
-createTour();
-getNextStage();
-
-var Player = ["PlayerId", "PlayerName"];
-
-function createTour(players) {
-    var testplayers = [
-        ["player/1", "Name1"],
-        ["player/2", "Name2"],
-        ["player/3", "Name3"],
-        ["player/4", "Name4"],
-        ["player/5", "Name5"],
-        ["player/6", "Name6"],
-        ["player/7", "Name7"],
-        ["player/8", "Name8"],
-        ["player/9", "Name9"],
-        ["player/10", "Name10"],
-        ["player/11", "Name11"],
-    ];
-    
-    var emptyPlayer = ["Empty", "Player"]
-    
-    players = testplayers;
-    
-    // random
-    
-    var matches = [];
-    
-    var count = players.length;
-    var next = Math.pow(2, Math.ceil(Math.log(count) / Math.log(2)));
-    
-    for (var i = 0; i < count; i++) {
-        if (i < next / 2) {
-            matches.push([testplayers[i], testplayers[i + 1]]);
-            i++;
-        } else {
-            matches.push([testplayers[i], emptyPlayer]);
-        }
-    }
-    console.log("CREATE TOUR:");
-    console.log(" stage = " + matches);
-}
 
 function getNextStage(players) {
     var testplayers = [
