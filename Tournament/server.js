027216edbe29ac3d5cf9b927878ab3a3528616b0
aviviadi <avivi.adi.hr@gmail.com>
2015-12-06 16:49:54 +0200

    calc matches

--- Tournament/server.js
+++ Tournament/server.js
@@ -71,3 +71,79 @@ var server = app.listen(port, function () {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Worfld\n');
 }).listen(port);*/
+
+
+console.log('Hello world');
+
+createTour();
+getNextStage();
+
+var Player = ["PlayerId", "PlayerName"];
+
+function createTour(players) {
+    var testplayers = [
+        ["player/1", "Name1"],
+        ["player/2", "Name2"],
+        ["player/3", "Name3"],
+        ["player/4", "Name4"],
+        ["player/5", "Name5"],
+        ["player/6", "Name6"],
+        ["player/7", "Name7"],
+        ["player/8", "Name8"],
+        ["player/9", "Name9"],
+        ["player/10", "Name10"],
+        ["player/11", "Name11"],
+    ];
+    
+    var emptyPlayer = ["Empty", "Player"]
+    
+    players = testplayers;
+    
+    // random
+    
+    var matches = [];
+    
+    var count = players.length;
+    var next = Math.pow(2, Math.ceil(Math.log(count) / Math.log(2)));
+    
+    for (var i = 0; i < count; i++) {
+        if (i < next / 2) {
+            matches.push([testplayers[i], testplayers[i + 1]]);
+            i++;
+        } else {
+            matches.push([testplayers[i], emptyPlayer]);
+        }
+    }
+    console.log("CREATE TOUR:");
+    console.log(" stage = " + matches);
+}
+
+function getNextStage(players) {
+    var testplayers = [
+        ["player/1", "Name1"],
+        ["player/3", "Name3"],
+        ["player/5", "Name5"],
+        ["player/7", "Name7"],
+        ["player/9", "Name9"],
+        ["player/10", "Name10"],
+        ["player/11", "Name11"],
+    ];
+    
+    var emptyPlayer = ["Empty", "Player"]
+    
+    players = testplayers;
+    
+    var matches = [];
+    var half = players.length / 2;
+    
+    for (var i = 0; i < (half / 2) + 1; i++) {
+        matches.push(players[i], players[players.length - i - 1])
+    }
+    var chk = half - Math.floor(players.length / 2);
+    if (chk !== 0) {
+        matches.unshift([players[Math.floor(players.length / 2)], emptyPlayer]);
+    }
+    
+    
+    console.log(" stage = " + matches);
+}
