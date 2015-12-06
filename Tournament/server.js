23d4cb479eef5f7d90c1a6f07d1ab1c11178ee98
Fitzchak Yitzchaki <fitzchak@ayende.com>
2015-12-06 13:52:37 +0200

    Initial code

---
+++ Tournament/server.js
@@ -0,0 +1,25 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	var players = ["fitzchak", "fff"];
+
+    var tourmanent = [["a"]];
+
+	res.send('Hello World!');
+});
+
+var server = app.listen(port, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log('Example app listening at http://%s:%s', host, port);
+});
+
+/*http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Worfld\n');
+}).listen(port);*/
